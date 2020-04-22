f8eabe458ca580f19997a7302f7dc9fec04f99e4
Divya Sharma <divds2904@gmail.com>
2020-04-22 11:39:02 +0530

    Updates

--- precache-manifest.468579c0f3e61fa1666a3fbb6794f489.js
+++ precache-manifest.468579c0f3e61fa1666a3fbb6794f489.js
@@ -1,31 +1,23 @@
 self.__precacheManifest = [
   {
-    "revision": "abe7778478377aaab4e2",
+    "revision": "8e72cbe5dc830506c7f1",
     "url": "/Shopping-Cart/static/css/main.6b64a9ab.chunk.css"
   },
   {
-    "revision": "abe7778478377aaab4e2",
-    "url": "/Shopping-Cart/static/js/main.abe77784.chunk.js"
+    "revision": "8e72cbe5dc830506c7f1",
+    "url": "/Shopping-Cart/static/js/main.8e72cbe5.chunk.js"
   },
   {
-    "revision": "3406a8ebf1801db8f93c",
+    "revision": "0dc73131103245a38a96",
     "url": "/Shopping-Cart/static/css/1.fab75533.chunk.css"
   },
   {
-    "revision": "3406a8ebf1801db8f93c",
-    "url": "/Shopping-Cart/static/js/1.3406a8eb.chunk.js"
+    "revision": "0dc73131103245a38a96",
+    "url": "/Shopping-Cart/static/js/1.0dc73131.chunk.js"
   },
   {
     "revision": "c3d4aa76f36cc5f07643",
     "url": "/Shopping-Cart/static/js/runtime~main.c3d4aa76.js"
-  },
-  {
-    "revision": "af7ae505a9eed503f8b8e6982036873e",
-    "url": "/Shopping-Cart/static/media/fontawesome-webfont.af7ae505.woff2"
-  },
-  {
-    "revision": "fee66e712a8a08eef5805a46892932ad",
-    "url": "/Shopping-Cart/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
     "revision": "674f50d287a8c48dc19ba404d20fe713",
@@ -36,11 +28,19 @@ self.__precacheManifest = [
     "url": "/Shopping-Cart/static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Shopping-Cart/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
     "revision": "912ec66d7572ff821749319396470bde",
     "url": "/Shopping-Cart/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
-    "revision": "5a2a2edd07342aa4bfb6a548db8ec60b",
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Shopping-Cart/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "d3a9d9115965ca6f3770d885fba24ec9",
     "url": "/Shopping-Cart/index.html"
   }
 ];
