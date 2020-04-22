ac4557c98962e1cfeae659b0efc0bc7bee6306cd
Divya Sharma <divds2904@gmail.com>
2020-04-22 10:41:45 +0530

    Updates

---
+++ precache-manifest.447d19e41d83d2123446392e6dcfd2f5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "abe7778478377aaab4e2",
+    "url": "/Shopping-Cart/static/css/main.6b64a9ab.chunk.css"
+  },
+  {
+    "revision": "abe7778478377aaab4e2",
+    "url": "/Shopping-Cart/static/js/main.abe77784.chunk.js"
+  },
+  {
+    "revision": "3406a8ebf1801db8f93c",
+    "url": "/Shopping-Cart/static/css/1.fab75533.chunk.css"
+  },
+  {
+    "revision": "3406a8ebf1801db8f93c",
+    "url": "/Shopping-Cart/static/js/1.3406a8eb.chunk.js"
+  },
+  {
+    "revision": "c3d4aa76f36cc5f07643",
+    "url": "/Shopping-Cart/static/js/runtime~main.c3d4aa76.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Shopping-Cart/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Shopping-Cart/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Shopping-Cart/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Shopping-Cart/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Shopping-Cart/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "5a2a2edd07342aa4bfb6a548db8ec60b",
+    "url": "/Shopping-Cart/index.html"
+  }
+];
