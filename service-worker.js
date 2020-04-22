78378938178a8d4daeacb4ffdfdb88c65c11cad2
Divya Sharma <divds2904@gmail.com>
2020-04-22 13:56:09 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c305912f9146441b927a5093ece54eb2.js"
+  "/precache-manifest.046b1d73e6dcde365b15153d0bf3c4fb.js"
 );
 
 workbox.clientsClaim();
