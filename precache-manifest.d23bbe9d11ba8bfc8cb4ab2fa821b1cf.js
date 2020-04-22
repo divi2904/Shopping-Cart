947ae3953aeef443dec9ce9ec350090f1af6c043
Divya Sharma <divds2904@gmail.com>
2020-04-22 14:02:30 +0530

    Updates

---
+++ precache-manifest.d23bbe9d11ba8bfc8cb4ab2fa821b1cf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e72cbe5dc830506c7f1",
+    "url": "/Shopping-Cart/static/css/main.6b64a9ab.chunk.css"
+  },
+  {
+    "revision": "8e72cbe5dc830506c7f1",
+    "url": "/Shopping-Cart/static/js/main.8e72cbe5.chunk.js"
+  },
+  {
+    "revision": "0dc73131103245a38a96",
+    "url": "/Shopping-Cart/static/css/1.fab75533.chunk.css"
+  },
+  {
+    "revision": "0dc73131103245a38a96",
+    "url": "/Shopping-Cart/static/js/1.0dc73131.chunk.js"
+  },
+  {
+    "revision": "c3d4aa76f36cc5f07643",
+    "url": "/Shopping-Cart/static/js/runtime~main.c3d4aa76.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Shopping-Cart/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Shopping-Cart/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Shopping-Cart/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Shopping-Cart/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Shopping-Cart/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "d3a9d9115965ca6f3770d885fba24ec9",
+    "url": "/Shopping-Cart/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1b10d1627afa556e8dc1ecc15117444a.js"
+  "/Shopping-Cart/precache-manifest.d23bbe9d11ba8bfc8cb4ab2fa821b1cf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Shopping-Cart/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
